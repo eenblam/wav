a5415eba679eb4f448858cee5993d4dac4371281
eenblam <ben.a.elam@gmail.com>
2020-11-10 12:54:33 -0800

    delay: add rough attempt at feedback

--- delay/main.py
+++ delay/main.py
@@ -90,10 +90,23 @@ def fade_in(t, attack_len, start=0, exp=1):
     # mid-fade-in
     return (1 - ((attack_len - t)) / attack_len) ** exp
 
+
+def interp(t, start=0, end=1, begin_time=0, finish_time=lindex):
+    length = finish_time - begin_time
+    if t < begin_time:
+        return start
+    if t >= finish_time:
+        return end
+    weight = (t - begin_time) / length
+    return start * (1 - weight) + end * weight
+
+
 #TODO (variable) feedback
-#TODO variable delay time
+#TODO improve variable delay time
+#TODO take a callback for a feedback FX send
+#TODO stereo spread
 class Delay():
-    def __init__(self, decay=0.5, max_delay_time=sample_rate):
+    def __init__(self, decay=0.5, feedback=0, max_delay_time=sample_rate):
         self.max_delay_time = int(max_delay_time)
         self.dtime = self.max_delay_time
         self.dbuff = [0] * self.max_delay_time
@@ -101,14 +114,21 @@ class Delay():
         self.r_head = 0
         self.w_head = self.max_delay_time - 1
         self.decay = decay
+        self.feedback = feedback
 
-    def send(self, signal, delay_time=None):
-        if delay_time is None:
-            delay_time = self.max_delay_time
+    def send(self, signal, feedback=0, delay_time=None):
+        if delay_time is not None:
+            if delay_time > self.max_delay_time:
+                raise ValueError("Delay time {} greater than max delay time {}".format(delay_time, self.max_delay_time))
+            self.dtime = int(delay_time)
+        if not (0 <= self.feedback <= 1):
+            raise ValueError("Feedback ({}) must be between 0 and 1".format(feedback))
+        self.feedback = feedback
         out = self.dbuff[self.r_head]
-        self.dbuff[self.w_head] += signal
         self.dbuff[self.w_head] *= self.decay
-        self.w_head = (self.w_head + 1) % self.dtime
+        self.dbuff[self.w_head] += unity(signal, out * self.feedback)
+        self.w_head = (self.w_head + 1) % self.max_delay_time
+        self.r_head = (self.w_head + self.dtime - 1) % self.max_delay_time
         self.r_head = (self.r_head + 1) % self.dtime
         return out
 
@@ -116,7 +136,7 @@ class Delay():
 def run(wav):
     rate = float(sample_rate)
 
-    d = Delay(max_delay_time=rate * 1.5)
+    d = Delay(decay=0.15, max_delay_time=rate * 2.5)
     delay = 0
     for i in range(int(length_seconds * sample_rate)):
         bass = volume * math.cos(bass_note(i) * math.pi * float(i) / rate)
@@ -124,9 +144,14 @@ def run(wav):
         env = bass_envelope(beat_pos)
         bass_out = env * bass
 
-        delay = d.send(bass_out) * fade_in(i, 5*rate, start=3*rate)
-        # Wait 3s, then fade in delay over 5s
-        if i < 3 * rate:
+        if i > sample_rate * 4:
+            bass_out = 0
+
+        dt = int(interp(i, start=rate*0.8, end=rate/2.5, begin_time=rate*3, finish_time=rate*5))
+        fb = 0.8
+        # Wait 1s, then fade in delay over 1s
+        delay = d.send(bass_out, feedback=fb, delay_time=dt) * fade_in(i, 1*rate, start=1*rate)
+        if i < rate:
             delay = 0
 
         l = unity(bass_out, delay)
